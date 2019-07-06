62d1be9cbecda522db7d1ace89f2f29adbfd8b6f
yshir <yshir0020@gmail.com>
2019-07-06 19:34:08 +0900

    change

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => (
-  <p>Hello, World. From react-codepipeline-s3.</p>
+  <div>
+    <p>Hello, World. From react-codepipeline-s3.</p>
+    <p style={{ color: 'red' }}>changed!!</p>
+  </div>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
